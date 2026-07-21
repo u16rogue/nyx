70f48b9bd491318277368fb3c9f2fc9cdfc13cc0
u16rogue <u16rogue@ushort.moe>
2026-07-21 09:44:53 +0000

    load custom script packages to `user`

--- nixos/users/user.nix
+++ nixos/users/user.nix
@@ -1,13 +1,31 @@
-{ config, self, ... }: let
+{ self, ... }: let
     username = "user";
 in {
-    flake.modules.nixos.users.${username} = { pkgs, ... }: {
+    flake.modules.nixos.users.${username} = { pkgs, ... }: let
+        mypkgs = self.packages.${pkgs.system};
+    in {
         users.users.${username} = {
             isNormalUser = true;
             extraGroups = [ "wheel" ];
-            shell = self.packages.${pkgs.system}.fish;
+            shell = mypkgs.fish;
             packages = [
-                config.users.users.user.shell
+                # Scripts
+                mypkgs.tmuxss
+                mypkgs.git-cans
+                mypkgs.mkignore
+                mypkgs.nix-develop
+                mypkgs.nix-gc
+                mypkgs.nix-pkgvercmp
+                mypkgs.nix-sync-lock-from-nixos
+
+                # Custom overridden packages (homeless configs +/ sandbox)
+                mypkgs.fish
+                mypkgs.nushell
+
+                # direct nixpkgs
+                pkgs.git
+                pkgs.jq
+                pkgs.bubblewrap
             ];
         };
     };
