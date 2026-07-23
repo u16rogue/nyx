8fdf8d1feaffeddd2c7dd6b55cf9f3c2fe2a0454
u16rogue <u16rogue@ushort.moe>
2026-07-23 13:10:25 +0000

    load users

--- nixos/users/default.nix
+++ nixos/users/default.nix
@@ -1,3 +1,10 @@
-{ ... }: {
-
+{ lib, ... }: {
+    imports = lib.pipe (builtins.readDir ./.) [
+        (lib.filterAttrs (name: value:
+            value == "regular"
+            && name != "default.nix"
+            && builtins.match ".*\\.nix" name != null))
+        builtins.attrNames
+        (builtins.map (name: ./. + "/${name}"))
+    ];
 }
