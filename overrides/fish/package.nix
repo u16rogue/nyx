720745f6fa1993c197aeb678c792da72c311d050
u16rogue <u16rogue@ushort.moe>
2026-08-24 16:25:41 +0000

    added `overridesOpts` to each `overrides/` package that allows consumers direct access to the available override options for customization

--- overrides/fish/package.nix
+++ overrides/fish/package.nix
@@ -1,7 +1,12 @@
-{ pkgs, fish, writeText, wrapPackage, ... }: wrapPackage {
+{ wrapPackage, pkgs, fish, writeText, overridesOpts ? {}, ... }:
+let
+    finalOpts = {
+        configFile = ./config.fish;
+    } // overridesOpts;
+in wrapPackage {
     inherit pkgs;
     package = fish;
     flags = {
-        "--init-command" = "source ${writeText "config.fish" (builtins.readFile ./config.fish)}";
+        "--init-command" = "source ${writeText "config.fish" (builtins.readFile finalOpts.configFile)}";
     };
 }
--- overrides/nushell/package.nix
+++ overrides/nushell/package.nix
@@ -1,7 +1,12 @@
-{ wrapPackage, pkgs, nushell, ... }: wrapPackage {
+{ wrapPackage, pkgs, nushell, overridesOpts ? {}, ... }:
+let
+    finalOpts = {
+        configFile = ./config.nu;
+    } // overridesOpts;
+in wrapPackage {
     inherit pkgs;
     package = nushell;
     flags = {
-        "--config" = ./config.nu;
+        "--config" = finalOpts.configFile;
     };
 }
