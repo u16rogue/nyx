0cbd997d1f5a06c0209921fcb212286f34816b01
u16rogue <u16rogue@ushort.moe>
2026-09-14 14:06:42 +0000

    [overrides/firefox] auto arrange toolbar; profile seeding to be an actual directory

--- overrides/firefox/package.nix
+++ overrides/firefox/package.nix
@@ -5,7 +5,6 @@
 # set tracking protection to strict
 # https only + private
 # dns over https
-# customize toolbar: [sidebars previous next refresh search/url downloads extension]
 # auto config sidebery
 { inputs, pkgs, lib, ... }: let
     addons = import ./addons.nix;
@@ -13,88 +12,75 @@
         name = "${name}-${addon.version}.xpi";
         inherit (addon) url sha256;
     });
-    profileSeed = pkgs.runCommand "nyx-firefox-profile-seed" {} ''
-        mkdir -p "$out/.mozilla/firefox/default/chrome"
-        cp ${./userChrome.css} "$out/.mozilla/firefox/default/chrome/userChrome.css"
-
-        cat > "$out/.mozilla/firefox/profiles.ini" <<'EOF'
-        [Profile0]
-        Name=default
-        IsRelative=1
-        Path=default
-        Default=1
-
-        [General]
-        StartWithLastProfile=1
-        Version=2
-        EOF
-    '';
-
-    initializedFirefox = inputs.wrappers.lib.wrapPackage {
-        inherit pkgs;
-        package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-            extraPolicies = {
-                DisableTelemetry = true;
-                DisableFirefoxStudies = true;
-                DisablePocket = true;
-                OfferToSaveLogins = false;
-
-                Preferences = {
-                    "toolkit.legacyUserProfileCustomizations.stylesheets" = {
-                        Value = true;
-                        Status = "user";
-                    };
-                    "browser.link.open_newwindow.restriction" = {
-                        Value = 0;
-                        Status = "user";
-                    };
-                    "privacy.userContext.newTabContainerOnLeftClick.enabled" = {
-                        Value = true;
-                        Status = "user";
-                    };
-                    "extensions.autoDisableScopes" = {
-                        Value = 0;
-                        Status = "user";
-                    };
-                    "extensions.activeThemeID" = {
-                        Value = addons.catppuccin.extid;
-                        Status = "user";
-                    };
-                };
-            };
-        };
-        wrapper = { exePath, ... }: /*bash*/ ''
-            profile="$HOME/.mozilla/firefox/default"
-            ${pkgs.coreutils}/bin/mkdir -p "$profile/chrome"
-
-            ${pkgs.coreutils}/bin/ln -sfnT "${profileSeed}/.mozilla/firefox/profiles.ini" \
-                "$HOME/.mozilla/firefox/profiles.ini"
-            ${pkgs.coreutils}/bin/ln -sfnT "${profileSeed}/.mozilla/firefox/default/chrome/userChrome.css" \
-                "$profile/chrome/userChrome.css"
-
-            extension_dir="$profile/extensions"
-            ${pkgs.coreutils}/bin/mkdir -p "$extension_dir"
-            ${pkgs.lib.concatStringsSep "\n" (lib.flip lib.mapAttrsToList addons (name: addon: /*bash*/ ''
-                if [[ ! -e "$extension_dir/${addon.extid}.xpi" ]]; then
-                    ${pkgs.coreutils}/bin/ln -s "${addonXpis.${name}}" "$extension_dir/${addon.extid}.xpi"
-                fi
-            ''))}
-
-            exec ${exePath} -P default "$@"
-        '';
-    };
-
+    profileSeed = ./profile-seed;
     mkNixPak = inputs.nixpak.lib.nixpak {
         inherit (pkgs) lib;
         inherit pkgs;
     };
 in (mkNixPak {
     config = { sloth, ... }: {
-        app = {
-            package = initializedFirefox;
-            binPath = "bin/firefox";
-        };
+        app.package = inputs.wrappers.lib.wrapPackage {
+            inherit pkgs;
+            package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+                extraPolicies = {
+                    DisableTelemetry = true;
+                    DisableFirefoxStudies = true;
+                    DisablePocket = true;
+                    OfferToSaveLogins = false;
 
+                    Preferences = {
+                        "toolkit.legacyUserProfileCustomizations.stylesheets" = {
+                            Value = true;
+                            Status = "user";
+                        };
+                        "browser.link.open_newwindow.restriction" = {
+                            Value = 0;
+                            Status = "user";
+                        };
+                        "privacy.userContext.newTabContainerOnLeftClick.enabled" = {
+                            Value = true;
+                            Status = "user";
+                        };
+                        "extensions.autoDisableScopes" = {
+                            Value = 0;
+                            Status = "user";
+                        };
+                        "extensions.activeThemeID" = {
+                            Value = addons.catppuccin.extid;
+                            Status = "user";
+                        };
+                    };
+                };
+            };
+            wrapper = { exePath, ... }: /*bash*/ ''
+                profile="$HOME/.mozilla/firefox/default"
+                ${pkgs.coreutils}/bin/mkdir -p "$profile/chrome"
+
+                toolbar_layout="$profile/.nyx-toolbar-layout"
+                # Do not write preferences while an existing Firefox owns this profile.
+                if [[ ! -e "$toolbar_layout" && ! -e "$profile/lock" && ! -e "$profile/.parentlock" ]]; then
+                    printf '\n' >> "$profile/prefs.js"
+                    ${pkgs.coreutils}/bin/cat "${profileSeed}/default/prefs.js" >> "$profile/prefs.js"
+                    ${pkgs.coreutils}/bin/touch "$toolbar_layout"
+                fi
+
+                ${pkgs.coreutils}/bin/ln -sfnT "${profileSeed}/profiles.ini" \
+                    "$HOME/.mozilla/firefox/profiles.ini"
+                ${pkgs.coreutils}/bin/ln -sfnT "${profileSeed}/default/chrome/userChrome.css" \
+                    "$profile/chrome/userChrome.css"
+
+                extension_dir="$profile/extensions"
+                ${pkgs.coreutils}/bin/mkdir -p "$extension_dir"
+                ${pkgs.lib.concatStringsSep "\n" (lib.flip lib.mapAttrsToList addons (name: addon: /*bash*/ ''
+                    if [[ ! -e "$extension_dir/${addon.extid}.xpi" ]]; then
+                        ${pkgs.coreutils}/bin/ln -s "${addonXpis.${name}}" "$extension_dir/${addon.extid}.xpi"
+                    fi
+                ''))}
+
+                exec ${exePath} -P default "$@"
+            '';
+        };
+        app.binPath = "bin/firefox";
         flatpak.appId = "org.mozilla.firefox";
         dbus.policies."org.freedesktop.portal.Desktop" = "talk";
         bubblewrap = {
