741ba93bf0a1a1c8e01a25fc7a367555cc5075d5
u16rogue <u16rogue@ushort.moe>
2026-09-21 02:36:33 +0000

    use seed copy instead of symlink for vesktop configs

--- overrides/vesktop/package.nix
+++ overrides/vesktop/package.nix
@@ -13,9 +13,14 @@ in (mkNixPak {
                 ${pkgs.coreutils}/bin/mkdir -p "$config_dir/settings"
                 ${pkgs.coreutils}/bin/ln -sfnT "${./discord-settings.json}" "$config_dir/settings.json"
                 ${pkgs.coreutils}/bin/ln -sfnT "${./quickCss.css}" "$config_dir/settings/quickCss.css"
-                [[ ! -e "$config_dir/state.json" ]] && ${pkgs.coreutils}/bin/ln -sfnT "${./state.json}" "$config_dir/state.json";
-                # These might be better as copy operations as it seems like vesktop does not do atomic replace
-                [[ ! -e "$config_dir/settings/settings.json" ]] && ${pkgs.coreutils}/bin/cp "${./vesktop-settings.json}" "$config_dir/settings/settings.json";
+                if [[ ! -e "$config_dir/state.json" ]]; then
+                    ${pkgs.coreutils}/bin/cp "${./state.json}" "$config_dir/state.json"
+                    ${pkgs.coreutils}/bin/chmod u+w "$config_dir/state.json"
+                fi
+                if [[ ! -e "$config_dir/settings/settings.json" ]]; then
+                    ${pkgs.coreutils}/bin/cp "${./vesktop-settings.json}" "$config_dir/settings/settings.json"
+                    ${pkgs.coreutils}/bin/chmod u+w "$config_dir/settings/settings.json"
+                fi
 
                 exec ${exePath} --enable-features=WebRTCPipeWireCapturer --ozone-platform=wayland "$@"
             '';
@@ -43,7 +48,7 @@ in (mkNixPak {
 
             bind.rw = [
                 [ (sloth.mkdir (sloth.concat [ (sloth.env "HOME") "/.nyx/app-fake-root/vesktop/" (sloth.env "HOME") ])) (sloth.env "HOME") ]
-                [ (sloth.mkdir "/tmp/.nyx-tmp/vesktop") "/tmp" ]
+                [ (sloth.mkdir (sloth.concat' sloth.runtimeDir "/nyx/vesktop")) "/tmp" ]
                 (sloth.concat' sloth.runtimeDir "/doc") # for document portal
             ];
 
