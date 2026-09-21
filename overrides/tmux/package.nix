eeebc521eb3e5b0ef4b297184f9e30c8f060444c
u16rogue <u16rogue@ushort.moe>
2026-09-21 02:51:16 +0000

    cleanup tmux; removed clanker hallucinated opt

--- overrides/tmux/package.nix
+++ overrides/tmux/package.nix
@@ -1,5 +1,9 @@
 { inputs, pkgs, tmux, ... }: inputs.wrappers.lib.wrapPackage {
     inherit pkgs;
     package = tmux;
-    flags."-f" = ./tmux.conf;
+    flags."-f" = pkgs.writeText "tmux.conf" (builtins.replaceStrings
+        [ "@tmuxPlugins.catppuccin@" ]
+        [ "${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux" ]
+        (builtins.readFile ./tmux.conf)
+    );
 }
