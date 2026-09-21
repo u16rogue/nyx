1ef7820966f4efa3deede3c5da357f39a233cad4
u16rogue <u16rogue@ushort.moe>
2026-09-21 02:04:15 +0000

    enforce openssh module to use the ed25519 key

--- nixos/modules/openssh.nix
+++ nixos/modules/openssh.nix
@@ -2,6 +2,10 @@
     flake.modules.nixos.openssh = {
         services.openssh = {
             enable = true;
+            hostKeys = [{
+                path = "/etc/ssh/ssh_host_ed25519_key";
+                type = "ed25519";
+            }];
             settings = {
                 PermitRootLogin = "no";
                 PasswordAuthentication = false;
